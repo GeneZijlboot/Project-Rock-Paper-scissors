6504cbe9ed775b8f0aeb30f08f798cd977bac79d
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-03-31 14:21:47 +0200

    rock paper scissors, one round

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const playerSelection = "Rock";
-function getComputerChoice(){
-    Array.prototype.random = function(){
-        return this[Math.floor((Math.random()*this.length))];
-    }
 
-    //var variables = ["Rock", "Paper", "Scissors"];
-    var list = ["Rock","Paper","Scissors"];
-    console.log(list.random());
+
+//chose random rock paper or scissors (computer)
+
+function getComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch(randomNumber) {
+    case 0:
+        return('rock');
+    case 1:
+        return('paper');
+    case 2:
+        return('scissors');
+    }
 }
 
 getComputerChoice();
+
+//use switch to figure out who wins when
+function playRound(playerSelection, computerSelection) {
+    if((playerSelection == 'rock') &&  (computerSelection == 'rock')){
+        return 'Tie!';
+    } else if((playerSelection == 'rock') && (computerSelection == 'paper')){
+        return 'you lose, paper beats rock';
+    }else if((playerSelection == 'rock') && (computerSelection == 'scissors')){
+        return 'you win, scissors beats rock';
+    }
+}
+
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
